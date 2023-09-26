a580d2c9f0cac7a0a897967be3893c026fce7f1c
andrew2719 <andrewblaze2719@gmail.com>
2023-09-26 12:59:28 +0530

    commited

--- Testing_2/PeerHandler.py
+++ Testing_2/PeerHandler.py
@@ -1,14 +1,77 @@
 import asyncio
 import aiofiles
+import asyncio
+import json
 import chunker
 
+import asyncio
+import json
+import chunker
+
+
 class PeerHandler:
-    def __init__(self,peer_connections,file_obj):
+    def __init__(self, peer_connections, file_obj):
         self.peer_connections = peer_connections
         self.file_obj = file_obj
-        self.chunk_table = chunker.Chunker(file_obj).chunk_file()
+        self.chunker = chunker.Chunker(file_obj)
+        self.chunk_table = None
 
-    async def UploadToPeer(self):
-        pass
+
+    async def generate_chunk_table(self):
+        self.chunk_table = await self.chunker.chunk_file()
+        '''chunk_table[i] = {
+                'index': i,
+                'hash': sha256,
+                'data': chunk,
+                'size': chunk_size,
+                'sent': False,
+                'peer': None
+            }
+        '''
+
+    async def UploadToPeer(self, peer, chunk):
+        reader, writer = self.peer_connections[peer]
+
+        # Preparing the file info for sending to the peer
+        file_info = {
+            "REQUEST": "UPLOAD",
+            "NODE": "NODE",
+            "file_name": chunk['hash'],
+            "file_size": chunk['size']
+        }
+
+        # Send the file info as JSON
+        writer.write(json.dumps(file_info).encode())
+        await writer.drain()
+
+        # Send the chunk data
+        writer.write(chunk['data'])
+        await writer.drain()
+
+        # Read acknowledgment from the peer
+        ack_data = await reader.read(1024)
+        ack = json.loads(ack_data.decode())
+
+        return (peer, ack['status'])
+
     async def UploadToPeers(self):
-        pass
+        if self.chunk_table is None:
+            await self.generate_chunk_table()
+
+        for i in self.chunk_table:
+            chunk = self.chunk_table[i]
+            peer = self.select_peer_for_chunk(i)
+            self.chunk_table[i]['peer'] = peer
+            peer, status = await self.UploadToPeer(peer, chunk)
+            if status == 'success':
+                self.chunk_table[i]['sent'] = True
+            else:
+                self.chunk_table[i]['sent'] = False
+
+
+    def select_peer_for_chunk(self, chunk_index):
+        # Logic to decide which peer receives which chunk
+        peer_keys = list(self.peer_connections.keys())
+        selected_peer = peer_keys[chunk_index % len(peer_keys)]
+        return selected_peer
+
--- Testing_2/chunker.py
+++ Testing_2/chunker.py
@@ -6,7 +6,7 @@ class Chunker:
         self.file_obj = file_obj
     async def chunk_file(self, num_chunks=8):
         data = self.file_obj.data
-        chunk_table = []
+        chunk_table = {}
         chunk_size = len(data) // num_chunks
         end_index = 0
         for i in range(num_chunks):
@@ -14,21 +14,27 @@ class Chunker:
             end_index = start_index + chunk_size
             chunk = data[start_index:end_index]
             sha256 = hashlib.sha256(chunk).hexdigest()
-            chunk_table.append({
+            chunk_table[i] = {
                 'index': i,
                 'hash': sha256,
-                'data': chunk
-            })
+                'data': chunk,
+                'size': chunk_size,
+                'sent': False,
+                'peer': None
+            }
 
         # If there's remaining data, add it as an extra chunk
         if len(data) % num_chunks != 0:
             extra_chunk = data[end_index:]
             sha256 = hashlib.sha256(extra_chunk).hexdigest()
-            chunk_table.append({
+            chunk_table[num_chunks] = {
                 'index': num_chunks,
                 'hash': sha256,
-                'data': extra_chunk
-            })
+                'data': extra_chunk,
+                'size': len(extra_chunk),
+                'sent': False,
+                'peer': None
+            }
 
         return chunk_table
 
