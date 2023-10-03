beeb5f92f4c1e42bb7887a0c77d079aba6ae589a
sandeepruttala <itssandeepruttala@gmail.com>
2023-10-03 12:36:30 +0530

    commit

--- reedsolo/rs.py
+++ reedsolo/rs.py
@@ -5,24 +5,26 @@
 from reedsolo import RSCodec
 
 # Create an RSCodec object with a specified number of error correction bytes (e.g., 2).
-rs = RSCodec(4)
+rs = RSCodec(2)
 
 # Your data to be encoded and potentially corrected.
 data = b"Hello, world!"
 
 # Encode the data with error correction.
 encoded_data = rs.encode(data)
+print("Encoded Data:", encoded_data)
 
 # Simulate some errors in the encoded data (this is just for demonstration purposes).
 corrupted_data = bytearray(encoded_data)
+print("Corrupted Data:", corrupted_data)
 corrupted_data[5] = 0  # Introducing an error by modifying a byte.
-
-corrupted_data[3] = 99  # Introducing an error by modifying a byte.
+print("Corrupted Data:", corrupted_data)
 
 # Attempt to decode and correct the corrupted data.
 try:
-    decoded_data = rs.decode(corrupted_data)    
-    corrected_data = decoded_data[0]
-    print("Corrected Data:", corrected_data.decode())
+    decoded_data = rs.decode(corrupted_data)
+    decoded_data = decoded_data[0]
+    print("Corrected Data:", decoded_data.decode())
 except ValueError:
     print("Failed to correct data. Too many errors.")
+
